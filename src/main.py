14f49831d46d7688d89154d577bd5424dd5eae2d
ugantarrajmanu <ugantarrajmanu@gmail.com>
2025-12-05 19:54:49 +0530

    update main.py to list all supported coins

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Query, HTTPException
 from src.config import settings
+from src.auth import verify_token
+
 
 app = FastAPI(
     title = settings.APP_NAME,
@@ -8,7 +10,7 @@ app = FastAPI(
     
 )
 
-@app.get("/health")
+@app.get("/health", tags=["Health"])
 async def health_check():
     try:
         await client._get("/ping")
@@ -24,3 +26,27 @@ async def health_check():
         "status": status,
         "upstream_service": upstream
     }
+
+@app.get("/api/v1/coins", dependencies=[Depends(verify_token)], tags=["Coins"])
+async def list_all_coins(
+    page: int = Query(1, ge=1, description="Page number"),
+    per_page: int = Query(10, ge=1, le=250, description="Items per page")
+):
+
+    try:
+        all_coins = await client.get_coin_list()
+        
+        start = (page - 1) * per_page
+        end = start + per_page
+        
+        paginated_items = all_coins[start:end]
+        
+        return {
+            "page": page,
+            "per_page": per_page,
+            "total_items": len(all_coins),
+            "data": paginated_items
+        }
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=str(e))
+
