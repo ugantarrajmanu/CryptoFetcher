324614f8ef1480ad4ba56377e39a8519c3a2d792
ugantarrajmanu <ugantarrajmanu@gmail.com>
2025-12-05 20:59:48 +0530

    fix test file

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from src.main import app
 from src.config import settings
 from unittest.mock import AsyncMock, patch
@@ -22,11 +22,13 @@ def mock_market_response_cad():
     return [{"id": "bitcoin", "symbol": "btc", "name": "Bitcoin", "current_price": 80000, "market_cap": 1600000}]
 
 
+
+
 @pytest.mark.asyncio
 async def test_health_check_success():
     with patch("src.main.client._get", new_callable=AsyncMock) as mock_get:
         mock_get.return_value = {"gecko_says": "(V3) To the Moon!"}
-        async with AsyncClient(app=app, base_url="http://test") as ac:
+        async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
             response = await ac.get("/health")
     
     assert response.status_code == 200
@@ -34,13 +36,13 @@ async def test_health_check_success():
 
 @pytest.mark.asyncio
 async def test_auth_missing():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/api/v1/coins")
-    assert response.status_code == 403 
+    assert response.status_code == 401
 
 @pytest.mark.asyncio
 async def test_auth_invalid():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/api/v1/coins", headers={"Authorization": "Bearer wrong"})
     assert response.status_code == 401
 
@@ -49,7 +51,7 @@ async def test_list_coins_pagination(valid_headers, mock_coin_list):
     with patch("src.main.client.get_coin_list", new_callable=AsyncMock) as mock_fetch:
         mock_fetch.return_value = mock_coin_list
         
-        async with AsyncClient(app=app, base_url="http://test") as ac:
+        async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
             # Request 1 item per page
             response = await ac.get("/api/v1/coins?page=1&per_page=1", headers=valid_headers)
             
@@ -73,7 +75,7 @@ async def test_market_data_integration(valid_headers, mock_market_response_inr, 
     with patch("src.main.client.get_market_data", new_callable=AsyncMock) as mock_service:
         mock_service.return_value = expected_result
         
-        async with AsyncClient(app=app, base_url="http://test") as ac:
+        async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
             response = await ac.get("/api/v1/coins/markets?ids=bitcoin", headers=valid_headers)
             
         assert response.status_code == 200
